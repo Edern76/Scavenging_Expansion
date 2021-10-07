7d5ab6c2aeb8c27953737adea1d7cf0354e6e38c
Gawein Le Goff <gawein.le@live.fr>
2021-10-07 15:24:26 +0200

    feat (jamming) : added unjam gizmo

--- Source/ScavengingExpansion/Comps/CompJammable.cs
+++ Source/ScavengingExpansion/Comps/CompJammable.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
 using Verse;
 
 namespace ScavengingExpansion.Comps
@@ -10,10 +13,71 @@ namespace ScavengingExpansion.Comps
         }
 
         public float JamChancePerShot = 0.05f;
+        public string UIIconPath = string.Empty;
     }
 
     public class CompJammable : ThingComp
     {
         public CompProperties_Jammable Props => (CompProperties_Jammable)this.props;
+        private Texture2D GizmoTex
+        {
+            get { Log.Message("Trying to find tex");return ContentFinder<Texture2D>.Get(Props.UIIconPath); }
+        }
+
+        private bool _jammed;
+
+        public bool Jammed
+        {
+            get { return _jammed; }
+        }
+        protected virtual Pawn GetWearer
+        {
+            get
+            {
+                if (ParentHolder != null && ParentHolder is Pawn_EquipmentTracker)
+                {
+                    return (Pawn)ParentHolder.ParentHolder;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public void setJammed()
+        {
+            this._jammed = true;
+        }
+
+        private void unjam()
+        {
+            this._jammed = false;
+        }
+
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            //if (GetWearer != null && GetWearer.IsColonist && Find.Selector.SingleSelectedThing == GetWearer)
+            //{
+                Log.Message("Getting gizmos");
+                if (Jammed)
+                {
+                    yield return new Command_Action
+                    {
+                        icon = this.GizmoTex,
+                        defaultLabel = "Unjam weapon",
+                        defaultDesc = "Unjam a jammed weapon",
+                        action = delegate { this.unjam(); },
+                        activateSound = SoundDef.Named("Click"),
+                    };
+                }
+                //}
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref this._jammed, "jammed", false);
+        }
     }    
 }
--- Source/ScavengingExpansion/Harmony/Patches/PatchVerbShoot.cs
+++ Source/ScavengingExpansion/Harmony/Patches/PatchVerbShoot.cs
@@ -29,20 +29,30 @@ namespace ScavengingExpansion.Harmony.Patches
             CompJammable compJam = equipment.TryGetComp<CompJammable>();
             if (compJam != null)
             {
-                float diceRoll = Rand.Value;
-                float chance = compJam.Props.JamChancePerShot;
-                #if DEBUG
-                    Log.Message($"DiceRoll : {diceRoll} | Chance : {chance}");
-                #endif
-                if (diceRoll >= chance)
+                if (!compJam.Jammed)
                 {
-                    return true; //No jamming, continue to vanilla method
+                    float diceRoll = Rand.Value;
+                    float chance = compJam.Props.JamChancePerShot;
+                    #if DEBUG
+                        Log.Message($"DiceRoll : {diceRoll} | Chance : {chance}");
+                    #endif
+                    if (diceRoll >= chance)
+                    {
+                        return true; //No jamming, continue to vanilla method
+                    }
+                    else
+                    {
+                        MoteMaker.ThrowText(launcher.DrawPos, launcher.Map, "*click*");
+                        compJam.setJammed();
+                            __result = false; //Cannot shoot
+                        return false; //Bypasses vanilla method, *hopefully* this shouldn't break things.
+                    }
                 }
                 else
                 {
                     MoteMaker.ThrowText(launcher.DrawPos, launcher.Map, "*click*");
-                    __result = false; //Cannot shoot
-                    return false; //Bypasses vanilla method, *hopefully* this shouldn't break things.
+                    __result = false;
+                    return false;
                 }
             }
             else
