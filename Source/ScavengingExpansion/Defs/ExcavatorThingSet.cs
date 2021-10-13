30ec4e29463999d8641c96f8b6743cdf777272ed
Edern <gawein.le@live.fr>
2021-10-13 14:40:28 +0200

    wip (excavator) : started implementing excavating

---
+++ Source/ScavengingExpansion/Defs/ExcavatorThingSet.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+using ScavengingExpansion.Defs.Tags;
+
+
+namespace ScavengingExpansion.Defs
+{
+    public class ExcavatorThingSet : ThingSet
+    {
+        public List<AdvancedScavengingProduct> excavatingProducts;
+    }
+}
