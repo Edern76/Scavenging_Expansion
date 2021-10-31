7e500cc2f5138a70f676970604330099d82158a2
Gawein Le Goff <gawein.le@live.fr>
2021-10-31 17:43:45 +0100

    feat (excavating) : excavating no longer allows all rocky tiles as valid

--- Source/ScavengingExpansion/Utils/Grids/ExcavatorGrid.cs
+++ Source/ScavengingExpansion/Utils/Grids/ExcavatorGrid.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Verse;
 
@@ -8,11 +10,13 @@ namespace ScavengingExpansion.Utils.Grids
     {
         private BoolGrid _boolGrid;
         private CellBoolDrawer _drawer;
+        private int _mapMaxClusters;
 
         public Color Color => Color.green; //Color to use for available cells display when placing building
         public bool GetCellBool(int index) => _boolGrid[index];
         public bool GetCellBool(IntVec3 cell) => _boolGrid[cell];
         public Color GetCellExtraColor(int index) => Color.white;
+        
 
         public ExcavatorGrid(Map map) : base(map)
         {
@@ -23,6 +27,10 @@ namespace ScavengingExpansion.Utils.Grids
         public override void FinalizeInit()
         {
             base.FinalizeInit();
+            _mapMaxClusters = Rand.RangeInclusive(1, 3); //TODO : Set min and max range in mod settings
+            #if DEBUG
+                Log.Message($"Max map clusters : {_mapMaxClusters}");
+            #endif
             if (_boolGrid.TrueCount == 0)
             {
                 CalculateBoolGrid();
@@ -38,7 +46,40 @@ namespace ScavengingExpansion.Utils.Grids
                     AddToGrid(cell, false); //TODO : Make it so only a few subsets of rocky tiles are valid
                 }    
             }
+            
+            List<List<IntVec3>> clusters = ExtractClusters();
+            List<IntVec3> finalCells = clusters.Take(_mapMaxClusters).SelectMany(x => x).ToList();
+
+            foreach (IntVec3 cell in GetCellsInGrid().Where(c => !finalCells.Contains(c)))
+            {
+                RemoveFromGrid(cell, false);
+            }
             _drawer.SetDirty();
+        }
+
+        private List<List<IntVec3>> ExtractClusters()
+        {
+            List<List<IntVec3>> clusters = new List<List<IntVec3>>();
+            List<IntVec3> clustersFlattened = new List<IntVec3>();
+            bool clustersModified = false;
+            foreach (IntVec3 cell in GetCellsInGrid())
+            {
+                if (clustersModified) //Avoids having to recalculate flatList for every cell
+                {
+                    clustersFlattened = clusters.SelectMany(x => x).ToList();
+                    clustersModified = false;
+                }
+
+                if (!clustersFlattened.Contains(cell))
+                {
+                    List<IntVec3> currentCluster = MapUtils.FloodFill(cell, map, GetCellBool);
+                    clusters.Add(currentCluster);
+                    clustersModified = true;
+                }    
+                   
+            }
+
+            return clusters;
         }
 
         public void AddToGrid(IntVec3 cell, bool isDirty = true)
@@ -63,6 +104,33 @@ namespace ScavengingExpansion.Utils.Grids
             }    
         }
 
+        public void RemoveFromGrid(IntVec3 cell, bool isDirty = true)
+        {
+            _boolGrid.Set(cell, false);
+            if (isDirty)
+            {
+                _drawer.SetDirty();
+            }    
+        }
+
+        public void RemoveFromGrid(CellRect rect, bool isDirty = true)
+        {
+            foreach (IntVec3 cell in rect.Cells)
+            {
+                RemoveFromGrid(cell, false);
+            }
+
+            if (isDirty)
+            {
+                _drawer.SetDirty();
+            }    
+        }
+
+        public IEnumerable<IntVec3> GetCellsInGrid()
+        {
+            return map.AllCells.Where(c => GetCellBool(c));
+        }
+
         public override void MapComponentUpdate()
         {
             base.MapComponentUpdate();
@@ -73,6 +141,7 @@ namespace ScavengingExpansion.Utils.Grids
         {
             base.ExposeData();
             Scribe_Deep.Look(ref _boolGrid, "boolGrid", new Object[0]); //Save/Load grid with current save
+            Scribe_Values.Look(ref _mapMaxClusters, "mapMaxClusters");
         }
 
         public void MarkForDraw()
