ae3d43df6c3e5cb6f49aa8f65d0de313cc5074d1
Gawein Le Goff <gawein.le@live.fr>
2021-11-06 17:39:55 +0100

    feat (blueprints) : added blueprints

---
+++ Source/ScavengingExpansion/Comps/CompAutoLearner.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using System.Linq;
+using ScavengingExpansion.Defs;
+using UnityEngine;
+using Verse;
+
+namespace ScavengingExpansion.Comps
+{
+    public class CompProperties_AutoLearner : CompProperties
+    {
+
+        public CompProperties_AutoLearner()
+        {
+            this.compClass = typeof(CompAutoLearner);
+        }
+
+        public string UIIconPath = "UI/Blueprint/Learn";
+        public bool SpawnWithSetProject = false;
+        public ResearchProjectSetDef AvailableProjects;
+    }
+
+    public class CompAutoLearner : ThingComp
+    {
+        public CompProperties_AutoLearner Props => (CompProperties_AutoLearner)this.props;
+
+        private List<ResearchProjectDef> ActuallyAvailableProjects =>
+            Props.AvailableProjects.projects.Where(proj => !proj.IsFinished).ToList();
+
+        private ResearchProjectDef setProject = null;
+        private Texture2D GizmoTex
+        {
+            get { return ContentFinder<Texture2D>.Get(Props.UIIconPath); }
+        }
+
+        private ResearchProjectDef getProjectFromSet()
+        {
+            List<ResearchProjectDef> projectPool = ActuallyAvailableProjects.Count > 0
+                ? ActuallyAvailableProjects
+                : Props.AvailableProjects.projects;
+
+            return projectPool[Rand.Range(0, projectPool.Count)];
+        }
+
+        private void UseItem()
+        {
+            ResearchProjectDef toLearn = this.setProject != null ? this.setProject : getProjectFromSet();
+            if (!toLearn.IsFinished)
+            {
+                Find.ResearchManager.FinishProject(toLearn);
+                parent.Destroy();
+            }
+            else
+            {
+                Log.Warning("Trying to learn already known project");
+            }
+        }
+
+        public override void Initialize(CompProperties props)
+        {
+            base.Initialize(props);
+            if (Props.SpawnWithSetProject)
+            {
+                this.setProject = getProjectFromSet();
+            }
+        }
+
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            yield return new Command_Action
+            {
+                icon = this.GizmoTex,
+                defaultLabel = "Use blueprint",
+                defaultDesc = "Learn the research project described in the blueprint",
+                action = this.UseItem,
+                activateSound = SoundDef.Named("Click"),
+                disabled = this.setProject?.IsFinished ?? getProjectFromSet() == null
+            };
+        }
+
+        public override string TransformLabel(string label) =>
+            this.setProject != null ? label + $" ({this.setProject.label})" : base.TransformLabel(label);
+    }
+}    
