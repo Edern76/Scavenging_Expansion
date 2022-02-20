70b009362761aca4021deaf261e98116eb8f7083
Gawein Le Goff <gawein.le@live.fr>
2022-02-20 16:04:50 +0100

    feat (blueprint) : added job to blueprint or survey data learning

--- Source/ScavengingExpansion/Comps/CompAutoLearner.cs
+++ Source/ScavengingExpansion/Comps/CompAutoLearner.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using RimWorld;
 using ScavengingExpansion.Defs;
 using ScavengingExpansion.Utils;
 using UnityEngine;
 using Verse;
+using Verse.AI;
+using JobDefOf = ScavengingExpansion.DefOfs.JobDefOf;
 
 namespace ScavengingExpansion.Comps
 {
@@ -18,6 +22,7 @@ namespace ScavengingExpansion.Comps
         public string UIIconPath = "UI/Blueprint/Learn";
         public bool SpawnWithSetProject = false;
         public ResearchProjectSetDef AvailableProjects;
+        public EffecterDef StudyingEffect;
     }
 
     public class CompAutoLearner : ThingComp
@@ -42,13 +47,15 @@ namespace ScavengingExpansion.Comps
             return projectPool[Rand.Range(0, projectPool.Count)];
         }
 
-        private void UseItem()
+        public void UseItem()
         {
             ResearchProjectDef toLearn = this.setProject != null ? this.setProject : getProjectFromSet();
             if (!toLearn.IsFinished)
             {
                 ResearchUtils.FinishProjectWithoutPrerequisites(toLearn);
+                Find.LetterStack.ReceiveLetter((TaggedString)$"{toLearn.label.CapitalizeFirst()} discovered", (TaggedString)$"By studying {this.parent.def.label}, your colonists have figured out the intricacies of {toLearn.label}.", LetterDefOf.PositiveEvent);
                 parent.Destroy();
+                
             }
             else
             {
@@ -64,24 +71,54 @@ namespace ScavengingExpansion.Comps
                 this.setProject = getProjectFromSet();
             }
         }
-
-        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        
+        public override IEnumerable<FloatMenuOption> CompFloatMenuOptions(Pawn selPawn)
         {
-            yield return new Command_Action
+            JobFailReason.Clear();
+            if (selPawn.WorkTypeIsDisabled(WorkTypeDefOf.Research) || selPawn.WorkTagIsDisabled(WorkTags.Intellectual))
+                JobFailReason.Is((string) "WillNever".Translate((NamedArgument) "Research".TranslateSimple().UncapitalizeFirst()));
+            else if (!selPawn.CanReach((LocalTargetInfo) (Thing) this.parent, PathEndMode.ClosestTouch, Danger.Some))
+                JobFailReason.Is((string) "CannotReach".Translate());
+            else if (!selPawn.CanReserve((LocalTargetInfo) (Thing) this.parent))
             {
-                icon = this.GizmoTex,
-                defaultLabel = "Use blueprint",
-                defaultDesc = "Learn the research project described in the blueprint",
-                action = this.UseItem,
-                activateSound = SoundDef.Named("Click"),
-                disabled = this.setProject?.IsFinished ?? getProjectFromSet() == null
-            };
+                Pawn pawn = selPawn.Map.reservationManager.FirstRespectedReserver((LocalTargetInfo) (Thing) this.parent, selPawn) ?? selPawn.Map.physicalInteractionReservationManager.FirstReserverOf((LocalTargetInfo) (Thing) selPawn);
+                if (pawn != null)
+                    JobFailReason.Is((string) "ReservedBy".Translate((NamedArgument) pawn.LabelShort, (NamedArgument) (Thing) pawn));
+                else
+                    JobFailReason.Is((string) "Reserved".Translate());
+            }
+
+            Job job = JobMaker.MakeJob(JobDefOf.SE_StudyBlueprint);
+            job.targetA = (LocalTargetInfo)(Thing)this.parent;
+            if (this.setProject?.IsFinished ?? getProjectFromSet() == null)
+            {
+                yield return new FloatMenuOption("You won't learn anything new from this", (Action)null);
+                JobFailReason.Clear();
+            }
+            else
+            {
+                if (JobFailReason.HaveReason)
+                {
+                    yield return new FloatMenuOption(
+                        (string)("CannotGenericWorkCustom".Translate(
+                                     (NamedArgument)"ApplyTechprint".Translate((NamedArgument)this.parent.Label)) +
+                                 ": " +
+                                 JobFailReason.Reason.CapitalizeFirst()), (Action)null);
+                    JobFailReason.Clear();
+                }
+                else
+                {
+                    yield return new FloatMenuOption($"Study {this.parent.def.label}",
+                        delegate() { selPawn.jobs.TryTakeOrderedJob(job); });
+                }
+            }
         }
+
 
         public override void PostExposeData()
         {
             base.PostExposeData();
-            Scribe_Values.Look(ref setProject, "setProject");
+            Scribe_Defs.Look(ref setProject, "setProject");
         }
 
 
